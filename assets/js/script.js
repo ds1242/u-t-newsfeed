b7fddbcf55d4fe604d1385868c331fbd5f72a308
ds1242 <david.shaw1242@gmail.com>
2023-02-12 11:21:10 -0700

    loaded json data to page cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,38 +3,41 @@
 
 const getLocalFeed = () => {
 
-    let output = fetch('../../feed.json')
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        for (let i = 0; i < data.length; i++) {
-            let title = data[i].title;
-            let link = data[i].link;
-            let date = data[i].date;
-            let contentSnippet = data[i].contentSnippet;
+    fetch('../../feed.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                let title = data[i].title;
+                let link = data[i].link;
+                let date = data[i].date;
+                let contentSnippet = data[i].contentSnippet;
 
-            $('#output-spot').append("<div class='card news-card'>" +
-                 "<h4 class='card-header' id='title'>" + title + "</h4>" +
-                 "<ul class='list-group-flush'>" +
+                $('#output-spot').append("<div class='card news-card'>" +
+                    "<div class='card-header'>" +
+                    "<div class='card-body'>" +
+                    "<h4 class='card-title' id='title'>" + title + "</h4>" +
+                    "</div>" +
+                    "<ul class='list-group-flush'>" +
                     "<li class='list-group-item'>" + link + "</li>" +
-                    "<li class='list-group-item'> "+ date + "</li>" +
+                    "<li class='list-group-item'> " + date + "</li>" +
                     "<li class='list-group-item'>" + contentSnippet + "</li>" +
-                 "</ul>" +
-            "</div>")
-            
-        }
-    })
+                    "</ul>" +
+                    "</div>" +
+                    "</div>")
+
+            }
+        })
 }
 
 
-const createCard = (data) => {
-    const newCard = document.createElement('div');
-    newCard.classList.add('card')
-    const createHeader = document.createElement('div')
-    createHeader.classList.add('card-header');
-    createHeader.textContent += data.title;
-    newCard.appendChild(createHeader)
-    
-}
-getLocalFeed()
+document.addEventListener("DOMContentLoaded", function () {
+    getLocalFeed();
+});
+
+const listButton = document.getElementById('list-button');
+const gridButton = document.getElementById('grid-button');
+
+
+
